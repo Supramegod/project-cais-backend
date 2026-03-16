1f202ce641c8ea28d830ed317f372779b84e8312
Jalu Pradipta <jluppradipta728@gmail.com>
2026-03-16 03:28:38 +0000

    Edit QuotationController.php

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -1551,7 +1551,7 @@ class QuotationController extends Controller
             ]);
         }
 
-        $approvalUrl = 'https://caisshelter.pages.dev/quotation/view/' . $quotation->id;
+        $approvalUrl = 'https://cais2.shelterapp2.co.id/quotation/view/' . $quotation->id;
         $this->quotationNotificationService->sendApprovalNotification(
             quotation: $quotation,
             creatorName: $creatorName,
